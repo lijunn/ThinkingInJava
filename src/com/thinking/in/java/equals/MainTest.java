8147d0de542f9b948cd4587a27448affa93e0efb
lijun <lijun@likingfit.com>
2019-07-30 17:05:26 +0800

    多线程

---
+++ src/com/thinking/in/java/equals/MainTest.java
@@ -0,0 +1,22 @@
+package com.thinking.in.java.equals;
+
+import javax.swing.tree.TreeNode;
+import java.lang.reflect.Array;
+import java.util.HashMap;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.println;
+
+/**
+ * @author : LiJun
+ * @date : 2019-07-15 15:08
+ **/
+public class MainTest {
+
+    public static void main(String[] args){
+
+        int num = 10;
+        int a = ++num;
+        System.out.println(" a = "+a+"\n num = "+num);
+
+    }
+}
