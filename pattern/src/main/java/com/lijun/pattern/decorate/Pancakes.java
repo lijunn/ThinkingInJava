fb18e3be7fcf3c9c4385d20728e2bf39ace0e6da
lijun <lijun@likingfit.com>
2020-04-10 18:37:30 +0800

    装饰模式

---
+++ pattern/src/main/java/com/lijun/pattern/decorate/Pancakes.java
@@ -0,0 +1,17 @@
+package com.lijun.pattern.decorate;
+
+/**
+ * @author : LiJun
+ * @date : 2020-04-10 16:05
+ **/
+public class Pancakes {
+
+    public String getName(){
+        return "煎饼";
+    }
+
+    public int getPrice(){
+        return 5;
+    }
+
+}
