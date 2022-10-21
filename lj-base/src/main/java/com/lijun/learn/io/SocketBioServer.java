5dc8d033bb190b475daa0c7f5a7b9668bc384d76
lijun <lijun@likingfit.com>
2022-10-21 19:29:09 +0800

    NIO

---
+++ lj-base/src/main/java/com/lijun/learn/io/SocketBioServer.java
@@ -0,0 +1,60 @@
+package com.lijun.learn.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author : LiJun
+ * @date : 2022-10-20 14:59
+ **/
+public class SocketBioServer {
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        try {
+            //监听端口
+            serverSocket = new ServerSocket(8090);
+
+            System.in.read();
+
+            while (true){
+                //阻塞获取 socket
+                Socket socket = serverSocket.accept();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            char[] data = new char[1024];
+
+                            while (true){
+                                int num = reader.read(data);
+                                if (num > 0){
+                                    System.out.println("Server receive data: "+ num + " value: "+new String(data,0,num));
+                                }else if (num == 0){
+                                    System.out.println("Server receive nothing");
+                                }else {
+                                    System.out.println("Server receive -1");
+                                    socket.close();
+                                    break;
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                assert serverSocket != null;
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
