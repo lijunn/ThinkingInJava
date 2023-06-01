8c528239a32c86eaeb4ebbbe675cf7f3de024026
lijun <lijun@likingfit.com>
2023-06-01 10:29:27 +0800

    设计模式

---
+++ lj-db/src/main/java/com/lijun/db/redis/RedisQueue.java
@@ -0,0 +1,8 @@
+package com.lijun.db.redis;
+
+/**
+ * @author : LiJun
+ * @date : 2023-06-01 10:19
+ **/
+public class RedisQueue {
+}
---
+++ lj-db/src/main/java/com/lijun/db/redis/RedisTest.java
@@ -0,0 +1,12 @@
+package com.lijun.db.redis;
+
+/**
+ * @author : LiJun
+ * @date : 2023-06-01 10:19
+ **/
+public class RedisTest {
+    public static void main(String[] args) {
+
+    }
+
+}
